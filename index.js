61a9a6fd3435ec2c7bd8c3f794286a4ebde25662
Abigail Amani Wanjiku <abigail.wanjiku@student.moringaschool.com>
2023-06-17 15:14:10 +0300

    advanced first class functios lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(-2)
+}
+
+
+const selectingDrivers = [
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers
+]
+
+
+
+
